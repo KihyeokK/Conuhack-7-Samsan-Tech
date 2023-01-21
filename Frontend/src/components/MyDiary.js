a8bcc3c27a3edcf2bb7d1f10e87be4d2dc8d3c8b
KihyeokK <joe.brighter20@gmail.com>
2023-01-21 18:20:25 -0500

    my work

--- Frontend/src/components/MyDiary.js
+++ Frontend/src/components/MyDiary.js
@@ -2,13 +2,21 @@ import DiaryList from "./DiaryList";
 import DiaryForm from "./DiaryForm";
 import Navbar from "./UI/Navbar";
 import styles from './MyDiary.module.css';
+import { useState, useEffect } from "react";
 
 const MyDiary = (props) => {
+
+  const [clickedDiaryEntry, setClickedDiaryEntry] = useState({});
+
+  const diaryClickHandler = (diaryEntry) => {
+    setClickedDiaryEntry(diaryEntry);
+  }
+
   return (
     <>
       <Navbar />
       <main className={styles["main-diary"]}>
-        <DiaryList />
+        <DiaryList onDiaryClick={diaryClickHandler}/>
         <DiaryForm></DiaryForm>
       </main>
     </>
