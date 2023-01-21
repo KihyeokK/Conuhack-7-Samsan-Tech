9ef02b23ca4c8d2109000823781debb39e1b50a8
KihyeokK <joe.brighter20@gmail.com>
2023-01-21 18:00:13 -0500

    trying to work on spotify api

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -8,6 +8,14 @@ function App() {
   const spotifyApi = new SpotifyWebApi();
   spotifyApi.setAccessToken("ebda86342f1f4cdd94ae46612d5318c5");
   
+  const searchMusic = async () => {
+    const searchResults = await spotifyApi.search("sad", ["playlist"]);
+    console.log(searchResults);
+  }
+  useEffect(()=> {
+    console.log("in App's useEffect");
+    searchMusic();
+  })
   return (
     <div className="App">
       {/* <h2>Welcome Back</h2>
