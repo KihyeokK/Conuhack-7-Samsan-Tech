d92d4f375a21b205f5ecf50a265af9b8a4a927fe
KihyeokK <joe.brighter20@gmail.com>
2023-01-22 05:39:15 -0500

    Fix Duplicate Files

--- Frontend/src/babel-plugin-macros.config 2.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'fontawesome-svg-core': {
-    'license': 'free'
-  }
-}
-
-
-  
--- Frontend/src/components/LoadMusicButton 2.js
+++
@@ -1,7 +0,0 @@
-
-const LoadMusicButton = props => {
-
-    return <button type="submit">load my music for today</button>
-}
-
-export default LoadMusicButton;
