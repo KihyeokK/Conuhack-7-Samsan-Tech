86c7a156a4588ac5b3858db389252e3c8b9bc323
Jaewon Moon <jaewonmoon@jaewons-mbp.wireless.concordia.ca>
2023-01-21 21:32:32 -0500

    fixed cors bug from signals.py

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'diaries',
     # django-rest-framework
     'rest_framework',
+    
+    'corsheaders',
 ]
 
 SITE_ID = 1
@@ -49,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'tutorial.urls'
